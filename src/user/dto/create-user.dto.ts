994b771414024a4ef5cd99b75bdbe29c425e0f8f
alexeyinn <alexey.i.nn@gmail.com>
2022-06-30 10:02:50 +0300

    feat: user registration validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IS_RFC_3339, Length } from "class-validator";
+
 export class CreateUserDto {
+  @Length(3, 32, {
+    message: "Имя пользователя должно быть от 3 до 32 символов!",
+  })
   fullName: string;
+
+  @IsEmail(undefined, {
+    message: "Проверьте указанный адрес почты! Неверный формат!",
+  })
   email: string;
+
+  @Length(6, 12, { message: "Пароль должен содержать от 3 до 32 символов!" })
   password?: string;
 }
